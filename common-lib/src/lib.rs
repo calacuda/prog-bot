f29d7338a4b989f67fd7001ab43cbac2e4a239c9
Eoghan West <eowest@gmail.com>
2024-07-07 20:04:16 -0400

    bare minimum common functions

--- common-lib/src/lib.rs
+++ common-lib/src/lib.rs
@@ -1,14 +1,86 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use anyhow::{bail, Result};
+use futures_util::{
+    stream::{SplitSink, SplitStream},
+    SinkExt, StreamExt,
+};
+use prog_bot_data_types::{
+    get_new_uuid, ProgBotMessage, ProgBotMessageContext, ProgBotMessageType, SubscribeTo, Uuid,
+};
+pub use tokio;
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio_tungstenite::{connect_async, tungstenite::Message, WebSocketStream};
+use tracing::*;
+
+/// returns user configs from config file
+pub fn get_config_file() {}
+
+/// returns the url for the message bus
+fn get_message_bus_url() -> String {
+    // TODO: call get_config_file
+    // TODO: build url from that
+
+    "ws://127.0.0.1:8080".into()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// connects to the message bus and returns the Uuid returned from the message_bus server
+pub async fn connect_to_messagebus(
+    sub_to: SubscribeTo,
+) -> Result<(
+    Uuid,
+    (
+        SplitSink<WebSocketStream<impl AsyncRead + AsyncWrite + Unpin>, Message>,
+        SplitStream<WebSocketStream<impl AsyncRead + AsyncWrite + Unpin>>,
+    ),
+)> {
+    let url = get_message_bus_url();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    info!("connecting to message bus at - {url}");
+    let (ws_stream, _) = connect_async(url).await?;
+    info!("connected to message bus successfully");
+
+    let (mut write, mut read) = ws_stream.split();
+
+    info!("attempting to register node");
+    write
+        .send(Message::Text(serde_json::to_string(&ProgBotMessage {
+            msg_type: ProgBotMessageType::Syn,
+            data: serde_json::to_value(&sub_to)?,
+            context: ProgBotMessageContext {
+                sender: get_new_uuid(),
+                response_to: None,
+            },
+        })?))
+        .await?;
+    info!("sent reqestration request.");
+
+    let uuid: Result<Uuid> = if let Some(message) = read.next().await {
+        match message {
+            Ok(msg) => match msg {
+                Message::Text(raw_msg) => {
+                    let msg: ProgBotMessage = serde_json::from_str(&raw_msg)?;
+
+                    if msg.msg_type == ProgBotMessageType::Ack {
+                        let uuid: Uuid = serde_json::from_value(msg.data)?;
+
+                        Ok(uuid)
+                    } else {
+                        bail!(
+                            "got wrogn response type from server. get {:?}",
+                            msg.msg_type
+                        )
+                    }
+                }
+                _ => bail!("got unexceptable response message type."),
+            },
+            Err(e) => {
+                error!("Error receiving message: {e}");
+                bail!("{e}")
+            }
+        }
+    } else {
+        bail!("failed to read message_buss response")
+    };
+    info!("got reqestration response");
+
+    Ok((uuid?, (write, read)))
 }
