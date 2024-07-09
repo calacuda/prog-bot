08a7922817dae1178ab81cc655ad2967ccaf8879
Eoghan West <eowest@gmail.com>
2024-07-08 20:39:32 -0400

    udpated nix dev flake

--- flake.nix
+++ flake.nix
@@ -3,30 +3,30 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-    # rust-overlay = {
-    #   url = "github:oxalica/rust-overlay";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
       overlays = [
-        # rust-overlay.overlays.default
-        # (final: prev: {
-        #   rustToolchain =
-        #     let
-        #       rust = prev.rust-bin;
-        #     in
-        #     if builtins.pathExists ./rust-toolchain.toml then
-        #       rust.fromRustupToolchainFile ./rust-toolchain.toml
-        #     else if builtins.pathExists ./rust-toolchain then
-        #       rust.fromRustupToolchainFile ./rust-toolchain
-        #     else
-        #       rust.stable.latest.default.override {
-        #         extensions = [ "rust-src" "rustfmt" ];
-        #       };
-        # })
+        rust-overlay.overlays.default
+        (final: prev: {
+          rustToolchain =
+            let
+              rust = prev.rust-bin;
+            in
+            if builtins.pathExists ./rust-toolchain.toml then
+              rust.fromRustupToolchainFile ./rust-toolchain.toml
+            else if builtins.pathExists ./rust-toolchain then
+              rust.fromRustupToolchainFile ./rust-toolchain
+            else
+              rust.stable.latest.default.override {
+                extensions = [ "rust-src" "rustfmt" ];
+              };
+        })
       ];
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
@@ -37,13 +37,14 @@
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            # rustToolchain
+            rustToolchain
             openssl
             pkg-config
             alsa-lib
             websocat
             rlwrap
             act
+            # cargo
             # cargo-deny
             # cargo-edit
             # cargo-watch
