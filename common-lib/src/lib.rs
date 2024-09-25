a7c7a57dd1a64a7a139948066966a78ba06c81f9
Eoghan West <eowest@gmail.com>
2024-09-25 14:30:49 -0400

    overhauled default logging

--- common-lib/src/lib.rs
+++ common-lib/src/lib.rs
@@ -1,8 +1,11 @@
+use std::fmt::Debug;
+
 use anyhow::{bail, Result};
 use futures_util::{
     stream::{SplitSink, SplitStream},
     SinkExt, StreamExt,
 };
+use instrument::WithSubscriber;
 use prog_bot_data_types::{
     Configuration, ProgBotMessage, ProgBotMessageContext, ProgBotMessageType, SubscribeTo, Uuid,
 };
@@ -10,6 +13,7 @@ pub use tokio;
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio_tungstenite::{connect_async, tungstenite::Message, WebSocketStream};
 use tracing::*;
+use tracing_subscriber::{filter::FilterFn, layer::SubscriberExt, prelude::*, Registry};
 
 // TODO: make logging macros that take a write connections to the message bus and a message to be
 // logged.
@@ -27,7 +31,9 @@ fn get_message_bus_url() -> String {
 }
 
 /// connects to the message bus and returns the Uuid returned from the message_bus server
+// TODO: add friendly name for loggin purposes
 pub async fn connect_to_messagebus(
+    // firendly
     sub_to: SubscribeTo,
 ) -> Result<(
     Uuid,
@@ -91,17 +97,27 @@ pub async fn connect_to_messagebus(
 }
 
 pub fn start_logging() -> Result<()> {
+    // let filter = tracing_subscriber::EnvFilter::new("actix!=info");
+    let filter = tracing_subscriber::filter::filter_fn(|metadata| {
+        !(metadata.target().starts_with("actix") && *metadata.level() == Level::TRACE)
+            && (!metadata.target().starts_with("mio"))
+    });
+
     // construct a subscriber that prints formatted traces to stdout
-    let subscriber = tracing_subscriber::fmt()
+    let fmt_layer = tracing_subscriber::fmt::layer()
         .compact()
         .with_thread_ids(true)
         .with_target(true)
         .with_level(true)
-        .with_max_level(Level::TRACE)
-        .without_time()
-        .finish();
+        .with_line_number(true)
+        .with_thread_names(true)
+        .without_time();
+
     // use that subscriber to process traces emitted after this point
-    tracing::subscriber::set_global_default(subscriber)?;
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(filter)
+        .try_init()?;
 
     Ok(())
 }
